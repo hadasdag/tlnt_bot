f18c8d6274ffe3692b54135406266a4793d6e8da
Hadas Dagan <hadasdag@gmail.com>
2016-12-03 23:11:57 +0200

    lala

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.post('/webhook', function (req, res) {
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
+    console.log('@@@', recipientId, message);
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
