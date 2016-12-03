bd4c725fe41929b323165a8eb4c9f8194f569dc0
Hadas Dagan <hadasdag@gmail.com>
2016-12-04 00:40:26 +0200

    lala

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var request = require('request');
 var app = express();
-var xml2js = require('xml2js');
+var bodyParser = require('body-parser');
+var express = require('express');
 var https = require('https');
-var xml = '';
+var request = require('request');
+var xml2js = require('xml2js');
 
 request.get('https://drive.google.com/uc?export=download&id=0B0Jkuy0hWLAMMU1RMmJHNUgyMHM', function (error, response, body) {
   console.log(body);
-  xml = body;
+  global.xml = body;
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,38 +16,38 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send(xml);
+  res.send(global.xml);
 });
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
-    console.log('@@@', req.body);  
-    var events = req.body.entry[0].messaging;
-    for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
+  console.log('@@@', req.body);  
+  var events = req.body.entry[0].messaging;
+  for (i = 0; i < events.length; i++) {
+    var event = events[i];
+    if (event.message && event.message.text) {
+      sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
     }
-    res.sendStatus(200);
+  }
+  res.sendStatus(200);
 });
 
 // generic function sending messages
 function sendMessage(recipientId, message) {
-    console.log('@@@', recipientId, message);
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error, recipientId, message);
-        }
-    });
+  console.log('@@@', recipientId, message);
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error, recipientId, message);
+    }
+  });
 };
